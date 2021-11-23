49e036e66c6530153afde829cd0befaad6a8e54f
Samiah Green <stu.samiah.green@ihsnola.org>
2021-11-23 18:41:05 +0000

    add five circles to project

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -25,8 +25,7 @@ var init = function (window) {
 
         // TODO 2 : Create a function that draws a circle
         function drawCircle() {
-            // Code to draw a circle
-circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
 physikz.addRandomVelocity(circle, canvas);
 view.addChild(circle);
 circles.push(circle);
@@ -35,6 +34,13 @@ circles.push(circle);
 
         // TODO 3 / 8 : Call the drawCircle() function
         drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        
+
+
 
 
 
@@ -49,25 +55,23 @@ circles.push(circle);
         */
         function update() {
             // TODO 4 : Update the circle's position //
+            physikz.updatePosition(circles[0]);
+            physikz.updatePosition(circles[1]);
+            physikz.updatePosition(circles[2]);
+            physikz.updatePosition(circles[3]);
+            physikz.updatePosition(circles[4]);
 
-	physikz.updatePosition( /* Your Bracket Notation HERE */ );
-	physikz.updatePosition( /* Your Bracket Notation HERE */ );
-	physikz.updatePosition( /* Your Bracket Notation HERE */ );
-	physikz.updatePosition( /* Your Bracket Notation HERE */ );
-	physikz.updatePosition( /* Your Bracket Notation HERE */ );
-            
+
+
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-
-game.checkCirclePosition( /* Your Bracket Notation HERE */ );
-game.checkCirclePosition( /* Your Bracket Notation HERE */ );
-game.checkCirclePosition( /* Your Bracket Notation HERE */ );
-game.checkCirclePosition( /* Your Bracket Notation HERE */ );
-game.checkCirclePosition( /* Your Bracket Notation HERE */ );
-            // TODO 9 : Iterate over the array
-           
+            game.checkCirclePosition(circles[0]);
+            game.checkCirclePosition(circles[1]);
+            game.checkCirclePosition(circles[2]);
+            game.checkCirclePosition(circles[3]);
+            game.checkCirclePosition(circles[4]);
             
-        }
-    
+            // TODO 9 : Iterate over the array
+
         /* 
         This Function should check the position of a circle that is passed to the 
         Function. If that circle drifts off the screen, this Function should move
@@ -108,4 +112,5 @@ if((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = init;
+    }
 }
