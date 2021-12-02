685ffb35748bbec91287dcf900aa04d76f47f518
Samiah Green <stu.samiah.green@ihsnola.org>
2021-12-02 14:47:25 +0000

    add five circles project

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -26,17 +26,17 @@ var init = function (window) {
         // TODO 2 : Create a function that draws a circle
         function drawCircle() {
             circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
-            physikz.addRandomVelocity(circle, canvas, 10, 10);
+            physikz.addRandomVelocity(circle, canvas);
             view.addChild(circle);
             circles.push(circle);
         }
-        
 
         // TODO 3 / 8 : Call the drawCircle() function
-        for (var loopsCompleted = 0; loopsCompleted < 100; loopsCompleted++) {
-            drawCircle();
-        }
-        
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
 
 
 
@@ -53,16 +53,19 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            
+            physikz.updatePosition(circles[0]);
+            physikz.updatePosition(circles[1]);
+            physikz.updatePosition(circles[2]);
+            physikz.updatePosition(circles[3]);
+            physikz.updatePosition(circles[4]);
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-            
+            game.checkCirclePosition(circles[0])
+            game.checkCirclePosition(circles[1])
+            game.checkCirclePosition()
+            game.checkCirclePosition()
+            game.checkCirclePosition()
             // TODO 9 : Iterate over the array
-            for (var i = 0; i < circles.length; i++) {
-                var eachCircle = circles[i];
-                physikz.updatePosition(eachCircle);
-                game.checkCirclePosition(eachCircle);
-                
-            }
+            
          
         /* 
         This Function should check the position of a circle that is passed to the 
@@ -77,18 +80,7 @@ var init = function (window) {
             }
             
             // TODO 7 : YOUR CODE STARTS HERE //////////////////////
-            // if(circle.x < 0) {
-            //     circle.x = 0;
-            // }
-
-            if (circle.y > canvas.height) {
-                circle.y = 0;
-            }
-
-            // if (circle.y < 0) {
-            //     circle.y = 0;
-            // }
-
+            
             // YOUR TODO 7 CODE ENDS HERE //////////////////////////
         }
         
